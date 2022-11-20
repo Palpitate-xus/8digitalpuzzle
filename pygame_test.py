2e1c67b14c8ce5679b1f7f709b802a12bd292fc6
Palpitate-xus <xus1758011080@outlook.com>
2022-11-20 15:23:15 +0800

    完成操作功能

--- pygame_test.py
+++ pygame_test.py
@@ -1,33 +1,119 @@
 import sys
 import pygame
-
+import random
 pygame.init()
 
-screen = pygame.display.set_mode((400,400))
+screen = pygame.display.set_mode((380, 380))
 screen.fill((255,255,255))
 pygame.display.set_caption('8 digital puzzle')
 
-f = pygame.font.Font('C:/Windows/Fonts/simhei.ttf',50)
-# 生成文本信息，第一个参数文本内容；第二个参数，字体是否平滑；
-# 第三个参数，RGB模式的字体颜色；第四个参数，RGB模式字体背景颜色；
-text0 = pygame.image.load("./digital/0.png").convert()
-text1 = pygame.image.load("./digital/1.png").convert()
-text2 = f.render("2", False, (0, 0, 0))
-text3 = f.render("3", False, (0, 0, 0))
-text4 = f.render("4", False, (0, 0, 0))
-text5 = f.render("5", False, (0, 0, 0))
-text6 = f.render("6", False, (0, 0, 0))
-text7 = f.render("7", False, (0, 0, 0))
-text8 = f.render("8", False, (0, 0, 0))
+block_size = 100
+block_gap  = 25
+top_screen = 0
+left_screen = 0
 
-#创建一个 50*50 的图像,并优化显示
-face = pygame.Surface((90, 90),flags=pygame.HWSURFACE)
-#填充颜色
-face.fill(color='pink')
+#颜色定义
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREY = (127, 127, 127)
+DEEPGREY = (89, 89, 89)
+FLESH1 = (255, 255, 229)
+FLESH2 = (255, 255, 204)
+RED1 = (255, 170, 128)
+RED2 = (255, 136, 77)
+RED3 = (255, 119, 51)
+RED4 = (255, 102, 26)
+RED5 = (255, 85, 0)
+YELLOW1 = (255, 255, 102)
+YELLOW2 = (255, 255, 51)
+YELLOW3 = (255, 255, 0)
+YELLOW4 = (232, 232, 10)
 
-# 固定代码段，实现点击"X"号退出界面的功能，几乎所有的pygame都会使用该段代码
+
+board = [[0, 0, 0],
+         [0, 0, 0],
+         [0, 0, 0]]
+
+def init():
+    global board
+    data = random.sample(range(9), 9)
+    print(data)
+    count = 0
+    for i in range(3):
+        for j in range(3):
+            board[i][j] = data[count]
+            count+=1
+    print(board)
+init()
+
+def draw_block():
+    global board
+    colors = {0:GREY, 1:FLESH1, 2:FLESH2, 3:RED1, 4:RED2, 5:RED3, 6:RED4, \
+              7:RED5, 8:YELLOW4}
+    x, y = left_screen, top_screen
+    size = block_size * 3 + block_gap * 4
+    pygame.draw.rect(screen, DEEPGREY, (x, y, size, size))
+    x, y = x + block_gap, y + block_gap
+    for i in range(3):
+        for j in range(3):
+            num = board[i][j]
+            if num == 0:
+                number = ""
+            else:
+                number = str(num)
+            color = colors[num]
+            class Block:
+                def __init__(self, topleft, number, color):
+                    self.topleft = topleft
+                    self.number = number
+                    self.color = color
+                def render(self, surface):
+                    x, y = self.topleft
+                    pygame.draw.rect(surface, self.color, (x, y, block_size, block_size))
+                    number_height  = int(block_size * 0.5)
+                    font = pygame.font.Font("2048reference/font/arial black.ttf", number_height)
+                    if num <= 3:
+                        number_surface = font.render(self.number, True, DEEPGREY)
+                    else:
+                        number_surface = font.render(self.number, True, WHITE)
+                    number_rect = number_surface.get_rect()
+                    number_rect.center = (x + block_size / 2, y + block_size / 2)
+                    surface.blit(number_surface, number_rect)
+            block = Block((x, y), number, color)
+            block.render(screen)
+            x += block_size + block_gap
+        x = left_screen + block_gap
+        y += block_size + block_gap
+
+
+def swap(a = [0, 0], b = [0, 0]):
+    global board
+    board[a[0]][a[1]], board[b[0]][b[1]] = board[b[0]][b[1]], board[a[0]][a[1]]
+
+def move(direct = 0):
+    global board
+    x, y = 0, 0
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == 0:
+                x, y = i, j
+    try:
+        if direct == 0:
+            swap([x, y], [x, y+1])
+        elif direct == 1 and y > 0:
+            swap([x, y], [x, y-1])
+        elif direct == 2:
+            swap([x, y], [x+1, y])
+        elif direct == 3 and x > 0:
+            swap([x, y], [x-1, y])
+
+    except:
+        print("error")
+
+# 固定代码段，实现点击"X"号退出界面的功能
 while True:
     # 循环获取事件，监听事件状态
+    draw_block()
     for event in pygame.event.get():
         # 判断用户是否点了"X"关闭按钮,并执行if代码段
         if event.type == pygame.QUIT:
@@ -38,19 +124,15 @@ while True:
         elif event.type == pygame.KEYDOWN :
             if event.key == pygame.K_UP:
                 print("up")
-                face.blit(text1, (0, 0))
+                move(2)
             if event.key == pygame.K_DOWN:
                 print("down")
-                face.fill(color='green')
-                face.blit(text3, (2, 0))
-            if event.key == pygame.K_RIGHT:
-                print("right")
-                face.fill(color='purple')
-                face.blit(text3, (2, 0))
+                move(3)
             if event.key == pygame.K_LEFT:
                 print("left")
-                face.fill(color='yellow')
-                face.blit(text4, (2, 0))
-    face.blit(text0, (0, 0))
-    screen.blit(face, (25, 25))
+                move(0)
+            if event.key == pygame.K_RIGHT:
+                print("right")
+                move(1)
     pygame.display.flip() #更新屏幕内容
+
--- tool.py
+++ tool.py
@@ -1,3 +1,2 @@
 for i in range(9):
-    print("block%s = tk.Button(mainWindow,text=\"%s\", font=20, width=10, height=4)"%(str(i), str(i)))
-    print("block%s.place(x=25, y=25)"%str(i))
+    print("block%s = pygame.Surface((100, 100),flags=pygame.HWSURFACE)"%(str(i)))
